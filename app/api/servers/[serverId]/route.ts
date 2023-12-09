5123e1874bc4c9bbe0becbb6a6a4a01866a61f32
debdut066 <nilkarpas@gmail.com>
2023-12-09 18:31:47 +0530

    server api

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,42 @@
+import { currentProfile } from "@/lib/current_profile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!params.serverId) {
+            return new NextResponse("Server Id Missing ", { status: 401 });
+        }
+
+        const server = await db.server.update({
+            where: {
+                id: params.serverId,
+                profileId: profile.id
+            },
+            data: {
+                inviteCode: uuidv4(),
+            },
+        });
+
+        return NextResponse.json(server);
+
+    } catch (error) {
+        console.log("[SERVER_ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
+
+function uuidv4(): any {
+    throw new Error("Function not implemented.");
+}
