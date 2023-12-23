0a3e1849e694da3a1e7cb3f7676cd839a3854ad8
debdut066 <nilkarpas@gmail.com>
2023-12-23 23:44:16 +0530

    chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,43 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current_profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+interface ChannelIdPageProps {
+    params : {
+        serverId : string;
+        channelId : string;
+    }
+}
 
-export default function ChannelIdPage(){
+async function ChannelIdPage({ params } : ChannelIdPageProps){
+
+    const profile = await currentProfile();
+
+    if(!profile){
+        return redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where : {
+            id : params.channelId
+        },
+    });
+
+    if(!channel){
+        return redirect("/")
+    }
+
     return(
-        <div>
-            Channel ID page
+        <div className="">
+            <ChatHeader
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
     )
 }
+
+export default ChannelIdPage;
