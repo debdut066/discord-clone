e0cafd2f20735827af204e4c5e52e8eb7d9bc444
debdut066 <nilkarpas@gmail.com>
2024-02-02 10:40:43 +0530

    bug fix

--- app/api/friendRequest/route.ts
+++ app/api/friendRequest/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import { currentProfile } from "@/lib/current_profile";
 import { db } from "@/lib/db";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const { name } = await req.json();
         const profile = await currentProfile();
